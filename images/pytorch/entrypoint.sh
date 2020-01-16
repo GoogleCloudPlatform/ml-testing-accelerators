821ce3e0cf6ceeb089c9657ee65306a02bbf4b9f
Will Cromar <wcromar@google.com>
2020-01-16 12:42:18 -0800

    Add PyTorch/XLA image.

---
+++ images/pytorch/entrypoint.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+set -e
+
+source /common.sh
+
+# Trim grpc:// prefix
+export XRT_TPU_CONFIG="tpu_worker;0;${KUBE_GOOGLE_CLOUD_TPU_ENDPOINTS:7}"
+
+set -u
+set -x
+
+docker-entrypoint.sh $@
+
+export PUBSUB_MESSAGE="{
+  \"model_dir\": \"$MODEL_DIR\",
+  \"logs_link\": \"$STACKDRIVER_LOGS\",
+  \"metric_collection_config\": $METRIC_COLLECTION_CONFIG,
+  \"regression_test_config\": $REGRESSION_TEST_CONFIG
+}"
+/root/google-cloud-sdk/bin/gcloud pubsub topics publish metrics-written --message "$PUBSUB_MESSAGE"
