7307a290fb0e7f7e1ec50b34d9f4be82ee5a03d4
Zachary Cain <zcain@google.com>
2020-01-30 11:49:12 -0800

    Add functionaltiy to clean up Stackdriver metrics.
    Change-Id: I17183d9ab06f076a20fa8200d0359976f6b2a9a4
    Reviewed-on: https://team-review.git.corp.google.com/c/cloud-accelerators-ml/xl-ml-test/+/653205
    Reviewed-by: Will Cromar <wcromar@google.com>

--- metrics_handler/cleanup.py
+++ metrics_handler/cleanup.py
@@ -21,14 +21,36 @@ def cleanup_alert_policies(test_name):
   print('Number of policies deleted: {}'.format(num_policies_deleted))
 
 
+def cleanup_metrics(test_name):
+  metric_client = monitoring_v3.MetricServiceClient()
+  project_name = metric_client.project_path(google.auth.default()[1])
+  num_metrics_deleted = 0
+  deleted_metrics = []
+  for x in metric_client.list_metric_descriptors(project_name):
+    if x.name.find(test_name) >= 0:
+      metric_client.delete_metric_descriptor(x.name)
+      num_metrics_deleted += 1
+      deleted_metrics.append(x.name)
+  print('Deleted metrics: {}'.format(deleted_metrics))
+  print('Number of metrics deleted: {}'.format(num_metrics_deleted))
+
+
 if __name__ == "__main__":
   parser = argparse.ArgumentParser(
           description='Batch delete of Stackdriver Alert Policies.',
           epilog=('Usage example: cleanup.py '
               '--test_name="pt-nightly-resnet50" '))
+  parser.add_argument('--type', type=str, default=None,
+                      help='Type of resource to clean up, e.g. "alerts" or '
+                      '"metrics".', required=True)
   parser.add_argument('--test_name', type=str, default=None,
                       help='Delete all alert policies that contain this '
                       'substring.', required=True)
   FLAGS = parser.parse_args()
-  cleanup_alert_policies(FLAGS.test_name)
+  if FLAGS.type == 'alerts':
+    cleanup_alert_policies(FLAGS.test_name)
+  elif FLAGS.type == 'metrics':
+    cleanup_metrics(FLAGS.test_name)
+  else:
+    raise ValueError('Unrecognized "type" argument: {}'.format(FLAGS.type))
 
