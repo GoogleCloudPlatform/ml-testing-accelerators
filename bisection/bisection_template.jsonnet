aafc4bf5b71dbf3737ae38dc5cf9838151de1024
zcain <zcain@google.com>
2020-08-26 11:52:32 -0700

    Add working version of runner and sample template.

---
+++ bisection/bisection_template.jsonnet
@@ -0,0 +1,42 @@
+local base = import 'templates/base.libsonnet';
+local tpus = import 'templates/tpus.libsonnet';
+local utils = import "templates/utils.libsonnet";
+local volumes = import "templates/volumes.libsonnet";
+
+local debugJob = base.BaseTest {
+  frameworkPrefix: "pt",
+  modelName: "custom-job",
+  mode: "bisection",
+  configMaps: [],
+
+  timeout: 3600, # 1 hour, in seconds
+
+  image: "gcr.io/tpu-pytorch/xla_debug",
+  imageTag: std.extVar('image-tag'),
+
+  tpuSettings+: {
+    softwareVersion: "pytorch-nightly",
+  },
+  accelerator: tpus.v3_8,
+
+  volumeMap+: {
+    datasets: volumes.PersistentVolumeSpec {
+      name: "pytorch-datasets-claim",
+      mountPath: "/datasets",
+    },
+  },
+  command: utils.scriptCommand(
+    |||
+      echo "In the VM now."
+      echo "KUBE_GOOGLE_CLOUD_TPU_ENDPOINTS: $KUBE_GOOGLE_CLOUD_TPU_ENDPOINTS"
+      export XRT_TPU_CONFIG="tpu_worker;0;${KUBE_GOOGLE_CLOUD_TPU_ENDPOINTS:7}"
+      echo "XRT_TPU_CONFIG: $XRT_TPU_CONFIG"
+      python3 pytorch/xla/test/test_train_mp_mnist.py --fake_data |& tee test_logs.txt
+      global_rate=$(grep 'GlobalRate=' test_logs.txt | tail -1 | grep -oP 'GlobalRate=([0-9]*[.])?[0-9]+ ' | sed 's/[^0-9.]*//g')
+      echo "Final GlobalRate was: $global_rate"
+      test $(echo "$global_rate 279.99" | awk '{print ($1 > $2)}') -eq 1
+    |||
+  ),
+};
+
+debugJob.oneshotJob
---
+++ bisection/run.sh
@@ -0,0 +1,106 @@
+# NOTE: This algorithm assumes that the test you supply follows the pattern of:
+#
+# fail, fail, fail, ..., pass, pass, pass, ...
+#
+# Where the 0 index is the latest image and the last index is the oldest
+# image. In other words, it is assumed that the test you write was passing for
+# some period of time and then switched to consistently failing. If the test
+# passes intermittently, this bisection won't work.
+IMAGE_NAME=gcr.io/tpu-pytorch/xla_debug
+MIN_TIMESTAMP='20200825_02_12'
+MAX_TIMESTAMP='20200825_08_00'
+TAG_PREFIX='nightly_3.6_'
+MAX_CHECKS=60  # Check 30 times w/ 30 sec wait = 1800s total.
+LOGS_DIR=bisection_logs/
+
+OLD_IFS=$IFS
+IFS=$'\n'
+
+# Collect all the Docker images that we want to try.
+# These will be appended with newer images as the first elements.
+images_to_try=()
+indices_already_tried=()
+for image in $(gcloud container images list-tags $IMAGE_NAME)
+do
+  timestamp=$(echo $image | awk '{ print $2 }')
+  timestamp=${timestamp#$TAG_PREFIX}
+  if [[ $timestamp =~ [0-9]{8}\_.* ]] && [[ "$timestamp" <  "$MAX_TIMESTAMP" ]] && [[ "$timestamp" >  "$MIN_TIMESTAMP" ]] ; then
+    images_to_try+=($(echo $image | awk '{ print $2 }'))
+    # Use a placeholder for the test result of each image.
+    indices_already_tried+=(-2)
+  fi
+done
+IFS=$OLD_IFS
+echo "List of image digests to try in bisection loop: ${images_to_try[@]}"
+echo "Indices already tried: ${indices_already_tried[@]}"
+hi=${#images_to_try[@]}
+lo=0
+mid=$(($hi/2))
+while true; do
+  echo "hi: $hi"
+  echo "lo: $lo"
+  echo "mid: $mid"
+  image_tag=${images_to_try[$mid]}
+  echo "Trying image with tag: $image_tag"
+  job_name=$(jsonnet -J ../ bisection_template.jsonnet --ext-str image-tag=$image_tag | kubectl create -f -)
+  job_name=${job_name#job.batch/}
+  job_name=${job_name% created}
+  echo "Waiting on kubernetes job: $job_name" && \
+  i=0 && \
+  status_code=2 && \
+  # Check on the job periodically. Set the status code depending on what
+  # happened to the job in Kubernetes. If we try max_checks times and
+  # still the job hasn't finished, give up and return the starting
+  # non-zero status code.
+  while [ $i -lt $MAX_CHECKS ]; do ((i++)); if kubectl get jobs $job_name -o jsonpath='Failed:{.status.failed}' | grep "Failed:1"; then status_code=1 && break; elif kubectl get jobs $job_name -o jsonpath='Succeeded:{.status.succeeded}' | grep "Succeeded:1" ; then status_code=0 && break; else echo "Job not finished yet"; fi; sleep 30; done && \
+  echo "Done waiting. Job status code: $status_code" && \
+  pod_name=$(kubectl get po -l controller-uid=`kubectl get job $job_name -o "jsonpath={.metadata.labels.controller-uid}"` | awk 'match($0,!/NAME/) {print $1}') && \
+  echo "GKE pod name: $pod_name" && \
+  kubectl logs -f $pod_name --container=train > "${LOGS_DIR}/${image_tag}.txt"
+  echo "Done with log retrieval attempt." && \
+  indices_already_tried[$mid]=$status_code
+
+  # Decide the index of the next image to try.
+  if [[ $status_code -eq 0 ]] ; then
+    # Test succeeded so we need to go further ahead time to find first failure.
+    # Images are ordered with newer images at lower index, so we lower the index.
+    hi=$mid
+    new_mid=$(((($hi+$lo)/2)))
+  else
+    # Test failed so we need to go further back in time to find earlier failure.
+    # Images are ordered with newer images at lower index, so we raise the index.
+    lo=$mid
+    new_mid=$(((($hi+$lo+1)/2)))
+  fi
+
+  echo "new_mid: $new_mid"
+  echo "Indices already tried: ${indices_already_tried[@]}"
+
+
+  # Check termination condition.
+  if [[ ${indices_already_tried[$new_mid]} -gt -2 ]] ; then
+    # If we've already tried this index, then it's time to stop.
+    # Find the earliest (i.e. highest index) image that was tried and returned
+    # a non-0 exit code.
+    max_failing_index=-1
+    for j in {0..${#images_to_try[@]}}
+    do
+      if [[ ${indices_already_tried[$new_mid]} -gt -2 ]] && [[ ${indices_already_tried[$new_mid]} -ne 0 ]] && [[ $j -gt $max_failing_index ]]; then
+        echo "NEW FAILING INDEX: $j"
+        max_failing_index=$j
+      fi
+    done
+
+    # Find the image associated with that index and report it.
+    if [[ $max_failing_index -gt -1 ]] ; then
+      echo "Earliest failing image was: ${images_to_try[$max_failing_index]}"
+    else
+      echo "No failing image was found."
+    fi
+    break
+  fi
+
+  # If we make it here, prepare to do another pass of the while loop.
+  mid=$new_mid
+done
+echo "See $LOGS_DIR for logs of all the attempted runs."
