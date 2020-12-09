e9df302799dff29fa68f6a19cd97828dae19c6de
Will Cromar <wcromar@google.com>
2020-12-09 15:05:49 -0800

    Run coverage in test script.

--- metrics_v2/run_tests.sh
+++ metrics_v2/run_tests.sh
@@ -2,9 +2,11 @@ set -x
 set -e
 
 PYTHONPATH=.
-python handler/utils_test.py
-python handler/alerts_test.py
-python handler/collectors/base_test.py
-python handler/collectors/literal_collector_test.py
-python handler/collectors/perfzero_collector_test.py
-python handler/collectors/tensorboard_collector_test.py
+coverage run -a handler/utils_test.py
+coverage run -a handler/alerts_test.py
+coverage run -a handler/collectors/base_test.py
+coverage run -a handler/collectors/literal_collector_test.py
+coverage run -a handler/collectors/perfzero_collector_test.py
+coverage run -a handler/collectors/tensorboard_collector_test.py
+
+coverage report -m
