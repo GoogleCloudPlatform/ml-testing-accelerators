fd97c50e7f333cc72ded285ba08ad77d73890ee9
Will Cromar <wcromar@google.com>
2020-12-17 14:18:14 -0800

    Update test script.

--- metrics_v2/run_tests.sh
+++ metrics_v2/run_tests.sh
@@ -4,11 +4,13 @@ set -e
 rm -f .coverage
 
 PYTHONPATH=handler/
+coverage run -a publisher/event_publisher_test.py
 coverage run -a -m handler.utils_test
 coverage run -a -m handler.alerts_test
 coverage run -a -m handler.collectors.base_test
 coverage run -a -m handler.collectors.literal_collector_test
 coverage run -a -m handler.collectors.perfzero_collector_test
 coverage run -a -m handler.collectors.tensorboard_collector_test
+coverage run -a integration_test.py
 
 coverage report -m
