f389ebb76e093cb1dd20cd42a5723936add1ecc0
Will Cromar <wcromar@google.com>
2021-06-25 11:45:17 -0700

    Delete Kubernetes workloads automatically when tests are removed. (#456)
    * Refactor utils.cronJobOutput.
    
    * Move cluster/accelerator config to separate file.
    
    * Delete Kubernetes workloads automatically when tests are removed.
    
    * Optimize splitByCluster

--- scripts/gen-tests.sh
+++ scripts/gen-tests.sh
@@ -6,5 +6,6 @@ set -e
 set -x
 which jsonnet &> /dev/null
 rm -f k8s/*/gen/*.yaml
-jsonnet -S -J . tests/multifile.jsonnet -m k8s/
+jsonnet -S -J . tests/cronjobs.jsonnet -m k8s/
+jsonnet -J . -S -m k8s/ tests/cleanup.jsonnet
 cd -
