b029c7c629d5a1ae060120c2685464221d8b8e01
Mohit Khatwani <mohitkhatwani@google.com>
2022-12-01 20:35:55 +0000

    add local device count

--- tests/multipods/pax/unit_tests/sync_devices.py
+++ tests/multipods/pax/unit_tests/sync_devices.py
@@ -3,6 +3,7 @@ import jax
 
 if __name__ == '__main__':
   print('Syncing %s global devices' % jax.device_count())
+  print('Local devices %s' % jax.local_device_count())
   # Block all hosts until directory is ready.
   py_utils.sync_global_devices('sync devices')
   print('Sync finished')
