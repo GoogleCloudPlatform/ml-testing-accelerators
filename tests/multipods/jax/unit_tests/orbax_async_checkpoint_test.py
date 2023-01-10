a2f0f84e7bae6dc7919db4341e015ce9836aedf3
Matthew Davidow <51136315+gobbleturk@users.noreply.github.com>
2023-01-09 17:19:02 -0800

    Update orbax_async_checkpoint_test.py

--- tests/multipods/jax/unit_tests/orbax_async_checkpoint_test.py
+++ tests/multipods/jax/unit_tests/orbax_async_checkpoint_test.py
@@ -39,21 +39,35 @@ dir_name = dir_name_flag.value
 ckpt_dir = bucket_path / dir_name
 print('orbax_async_checkpoint_test: Using ckpt_dir: ', ckpt_dir, flush=True)
 
+def broadcast_from_process_0(value):
+  """broadcasts non-negative value from process 0 to all processes"""
+  if jax.process_index() != 0:
+    value = 0
+  values_gathered = jax.experimental.multihost_utils.process_allgather(value)
+  return jax.numpy.max(values_gathered)
+
 def gen_local_ip():
-  return get_metadata('worker-network-endpoints').split(',')[0]
+  to_ret = get_metadata('worker-network-endpoints').split(',')[0] # Array with 1 element, we just grab it.
+  print("orbax_async_checkpoint_test: gen_local_ip: ", to_ret, flush=True)
+  return to_ret
 
-def gen_ip_nums():
-  return [int(num) for num in gen_local_ip().split(":")[-1].split(".")]
-
-def regather_ip_0(hosts_arr):
- return '.'.join([str(part[0]) for part in hosts_arr])
+def gen_local_ip_nums():
+  to_ret = [int(num) for num in gen_local_ip().split(":")[-1].split(".")]
+  print("orbax_async_checkpoint_test: gen_local_ip_nums: ", to_ret, flush=True)
+  return to_ret
 
 def get_coordinator_ip():
-  ip_nums= gen_ip_nums()
-  ip_nums_across_devices = [ip_nums.copy() for _ in range(len(jax.devices()))]
-  ip_dict = {'host_ips':ip_nums_across_devices}
-  ip_nums_gathered = jax.experimental.multihost_utils.process_allgather(ip_dict)
-  return regather_ip_0(ip_nums_gathered['host_ips'][0])
+  ip_nums= gen_local_ip_nums()
+  str_list = list()
+  # Very strange behavior is occasionaly been without the pause,
+  # The elements (typically the 2nd and 3rd) are sometimes read in the wrong order for 
+  # the other host.
+  for num in ip_nums:
+    broadcasted_num = broadcast_from_process_0(num)
+    time.sleep(1.0)
+    print("orbax_async_checkpoint_test: broadcasted ", broadcasted_num, flush=True)
+    str_list.append(str(broadcasted_num))
+  return '.'.join(str_list)
   
 def get_coordinator_address():
   host_0_ip = get_coordinator_ip()
