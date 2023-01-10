fb60f341f4c436d135ea645f8529283cec2a5a58
Matthew Davidow <51136315+gobbleturk@users.noreply.github.com>
2023-01-09 16:02:04 -0800

    Add flushed logging to orbax_async

--- tests/multipods/jax/unit_tests/orbax_async_checkpoint_test.py
+++ tests/multipods/jax/unit_tests/orbax_async_checkpoint_test.py
@@ -33,11 +33,11 @@ dir_name_flag = flags.DEFINE_string(
 
 FLAGS(sys.argv)  # parses the flags.
 
-print('orbax_async_checkpoint_test: Setting up ckpt_dir')
+print('orbax_async_checkpoint_test: Setting up ckpt_dir', flush=True)
 bucket_path = epath.Path(bucket_path_flag.value)
 dir_name = dir_name_flag.value
 ckpt_dir = bucket_path / dir_name
-print('orbax_async_checkpoint_test: Using ckpt_dir: ', ckpt_dir)
+print('orbax_async_checkpoint_test: Using ckpt_dir: ', ckpt_dir, flush=True)
 
 def gen_local_ip():
   return get_metadata('worker-network-endpoints').split(',')[0]
@@ -61,13 +61,13 @@ def get_coordinator_address():
   return host_0_ip + ":" + str(port)
 
 coordinator_address = get_coordinator_address()
-print('orbax_async_checkpoint_test: Using coordinator_address: ', coordinator_address)
+print('orbax_async_checkpoint_test: Using coordinator_address: ', coordinator_address, flush=True)
 
 jax.distributed.initialize(coordinator_address=coordinator_address,num_processes=jax.process_count(),process_id=jax.process_index())
-print('orbax_async_checkpoint_test: Initialized successful!')
+print('orbax_async_checkpoint_test: Initialized successful!', flush=True)
 
 mngr = orbax.CheckpointManager(ckpt_dir, orbax.AsyncCheckpointer(orbax.PyTreeCheckpointHandler()))
-print("orbax_async_checkpoint_test: Created Async CheckpointManager")
+print("orbax_async_checkpoint_test: Created Async CheckpointManager", flush=True)
 
 BATCH = 512
 SIZE = 128
@@ -89,13 +89,13 @@ gen_data_sharded = pjit(
 with maps.Mesh(mesh.devices, mesh.axis_names):
   presharded_X = jax.block_until_ready(gen_data_sharded())
   to_save = {"my_X": presharded_X}
-  print("orbax_async_checkpoint_test: Attempting save...")
+  print("orbax_async_checkpoint_test: Attempting save...", flush=True)
   mngr.save(0, to_save)
-  print("orbax_async_checkpoint_test: Save successful!")
+  print("orbax_async_checkpoint_test: Save successful!", flush=True)
 
 # Try to restore now
-print("orbax_async_checkpoint_test: Attempting restore...!")
+print("orbax_async_checkpoint_test: Attempting restore...!", flush=True)
 s2 = mngr.restore(0)
-print("orbax_async_checkpoint_test: Restore successful!")
+print("orbax_async_checkpoint_test: Restore successful!", flush=True)
 
-print("orbax_async_checkpoint_test: Test finished successfully")
+print("orbax_async_checkpoint_test: Test finished successfully", flush=True)
