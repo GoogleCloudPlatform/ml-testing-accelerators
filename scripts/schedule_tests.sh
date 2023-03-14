3c33073cf38cc55200b41887a08b105aaf89238e
Steven Krawczyk <steventk@google.com>
2023-03-14 03:41:48 +0000

    Add script to schedule tests

---
+++ scripts/schedule_tests.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+if [[ -z "${XLML_TEST_TYPE}" ]]; then
+    echo "Please set XLML_TEST_TYPE"
+    exit 1
+else
+    test_type="${XLML_TEST_TYPE}"
+fi
+
+regions=(us-central1 us-central2 europe-west4)
+for region in ${regions[@]}; do
+
+    gcloud container clusters get-credentials xl-ml-test-"$region" --region "$region" --project xl-ml-test
+    for i in $(kubectl get cronjobs -n automated -o name | grep "$test_type"); do
+        echo "$i"
+        jobName=$(echo "$i" | awk -F/ '{print $2}')
+        echo "Creating $jobName"
+        kubectl create job --from="$i" "$jobName" -n automated
+    done
+done
